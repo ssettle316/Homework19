e10856e37d78718f2f1be3c94ba3138ec986d386
ssettle316 <59590964+ssettle316@users.noreply.github.com>
2020-07-07 01:33:13 -0400

    Add files via upload

---
+++ src/components/DataTable.js
@@ -0,0 +1,18 @@
+import React from "react";
+import DataBody from "./DataBody";
+import "../styles/DataTable.css";
+
+function DataTable({ headings, users, handleSort }) {
+    {/* Here we will creat the header row for the table and using the values
+      from headings.
+    */
+  return (
+    <>
+    <h2>DataTable</h2>
+    </>
+  ) 
+  }
+
+}
+  // Hint: headings.map
+  export default DataTable;
